09756aeb2d5dfb48a81c1cd5ff24f8b53fd7ba5b
gr411_kanti <mrsteem28@mail.ru>
2023-03-06 16:19:42 +0700

    PR_2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "Nice Socks man...",
+        image: "./assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
     }
 })
