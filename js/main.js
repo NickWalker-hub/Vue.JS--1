ce78947cff9fc98a13f5b901becdebd18d330ff3
gr411_kanti <mrsteem28@mail.ru>
2023-03-07 15:48:12 +0700

    PR_7

--- js/main.js
+++ js/main.js
@@ -2,23 +2,27 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        cart: 0,
         altText: "A pair of socks",
-        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                onSale: true,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+                onSale: false,
             }
         ],
-        cart: 0,
     },
     methods: {
         addToCart() {
@@ -29,8 +33,24 @@ let app = new Vue({
                 this.cart -=1;
             }
         },
-        updateProduct(variantImage) {
-            this.image = variantImage;
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        },
+    },
+    computed: {
+        title() {
+            if (this.variants[this.selectedVariant].onSale){
+                return this.brand + ' ' + this.product + ' ' + '[Sale]';
+            }else {
+                return this.brand + ' ' + this.product + ' ' + '[Over]';
+            }
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity;
         },
     }
 })
